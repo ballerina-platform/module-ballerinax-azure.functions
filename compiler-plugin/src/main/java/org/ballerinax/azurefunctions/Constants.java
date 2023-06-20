bc0f144a983c54d024bb804859a8709bd2a0534c
xlight05 <anjanasupun05@gmail.com>
2023-06-20 20:31:33 +0530

    Fix local native build option

--- compiler-plugin/src/main/java/org/ballerinax/azurefunctions/Constants.java
+++ compiler-plugin/src/main/java/org/ballerinax/azurefunctions/Constants.java
@@ -86,11 +86,7 @@ public class Constants {
     public static final String REMOTE_KEYWORD = "remote";
     
     public static final String AZURE_FUNCTIONS_BUILD_OPTION = FUNCTION_DIRECTORY;
-    public static final String AZURE_FUNCTIONS_LOCAL_BUILD_OPTION = AZURE_FUNCTIONS_MODULE_NAME + "_local";
-    public static final String SERVICE_KEYWORD = "service";
-
-    public static final String AZF_SERVICE = AZURE_FUNCTIONS_MODULE_NAME + ":ResourceService";
-    public static final String DEFAULT = "default";
+    public static final String AZURE_FUNCTIONS_LOCAL_BUILD_OPTION = AZURE_FUNCTIONS_BUILD_OPTION + "_local";
     public static final String GET = "get";
     public static final String HEAD = "head";
     public static final String OPTIONS = "options";
